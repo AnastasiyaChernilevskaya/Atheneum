8b30ea9fe529f8ea6f97330d5379e38960e449e9
Anuitex-20 <Anuitex-20@DESKTOP-RUL97QM>
2017-11-20 19:38:41 +0200

    clean

--- Atheneum/Program.cs
+++ Atheneum/Program.cs
@@ -14,32 +14,6 @@ namespace Atheneum
 {
     public class Program
     {
-        //public static void Main(string[] args)
-        //{
-        //    var host = BuildWebHost(args);
-
-        //    using (var scope = host.Services.CreateScope())
-        //    {
-        //        var services = scope.ServiceProvider;
-        //        try
-        //        {
-        //            var context = services.GetRequiredService<AtheneumContext>();
-        //        }
-        //        catch (Exception ex)
-        //        {
-        //            var logger = services.GetRequiredService<ILogger<Program>>();
-        //            logger.LogError(ex, "An error occurred while seeding the database.");
-        //        }
-        //    }
-
-        //    host.Run();
-        //}
-
-        //public static IWebHost BuildWebHost(string[] args) =>
-        //    WebHost.CreateDefaultBuilder(args)
-        //        .UseStartup<Startup>()
-        //        .Build();
-
         public static void Main(string[] args)
         {
             BuildWebHost(args).Run();
